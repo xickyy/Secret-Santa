aa8e613e117788a734094de8be33ba021c01377f
ricky <ricky_moore@ymail.com>
2023-11-10 18:24:40 -0800

    initial commit....code already mostly functional though

---
+++ shuffle.js
@@ -0,0 +1,29 @@
+// Secret Santa Name Shuffler
+// ********INSTRUCTIONS*********
+
+// Input the Names of all the participants in both the buyers and recievers Arrays
+
+// Run it and let the code do what the code do :D
+
+
+let buyers = ['Ricky', 'Bobbie', 'Shelbi', 'Savannah', 'Hailey', 'Madison', 'Candace', 'Jose', 'Haim', 'Alyssa', 'Zaiden', 'Vivian', 'Lexi', 'Damien', 'Brian', 'Nick'];
+let recievers = ['Ricky', 'Bobbie', 'Shelbi', 'Savannah', 'Hailey', 'Madison', 'Candace', 'Jose', 'Haim', 'Alyssa', 'Zaiden', 'Vivian', 'Lexi', 'Damien', 'Brian', 'Nick'];
+
+const shuffle = (buyers, recievers) => {
+  for (let i = 0; i < buyers.length; i++) {
+
+    let name1 = buyers[i];
+    let logic = Math.floor(Math.random() * (0 - recievers.length) + recievers.length)
+    let name2 = recievers[logic]
+
+    while (name1 === name2) {
+      logic = Math.floor(Math.random() * (0 - recievers.length) + recievers.length)
+      name2 = recievers[logic]
+    }
+
+    recievers.splice(logic, 1)
+    console.log(`${name1} -------> ${name2}`);
+  }
+}
+
+  shuffle(buyers, recievers)
